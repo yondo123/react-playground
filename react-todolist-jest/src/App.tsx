42d03a91b4c870b00629bd4b50124bed3c35d1da
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-11-07 15:49:40 +0900

    ➖ remove: unused code and comments

--- react-todolist-jest/src/App.tsx
+++ react-todolist-jest/src/App.tsx
@@ -3,24 +3,12 @@ import { containerStyle } from '@shared/layouts/styles/containerStyle';
 import { Header } from '@shared/layouts/components/Header';
 import { TodoList } from '@todolist/components/TodoList';
 import { TodoListControls } from '@todolist/components/TodoListControls';
-import { useDispatch, useSelector } from 'react-redux';
-import { add } from './data/slice/todoSlice';
 import resetStyles from '@shared/styles/resetStyle';
-import type { RootState } from './data/configure';
-import { Tag } from '@shared/layouts/components/Tag';
 
 export const App = () => {
-  const dispatch = useDispatch();
-  const todos = useSelector((state: RootState) => state.todo.present);
-  console.log(todos);
-
-  const addTodo = () => {
-    dispatch(add({ title: '새로운 할 일', completed: false, id: '21', dueDate: new Date() }));
-  };
   return (
     <main css={containerStyle}>
       <Global styles={resetStyles} />
-      {/* <button onClick={addTodo}>투두추가</button> */}
       <Header />
       <TodoList />
       <TodoListControls />
