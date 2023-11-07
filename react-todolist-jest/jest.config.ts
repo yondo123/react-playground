fec342560d0814da233fc980180bb3d66f596211
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-11-07 15:53:42 +0900

    🤡 chore: add moduleNameMapper to jest.config.ts

--- react-todolist-jest/jest.config.ts
+++ react-todolist-jest/jest.config.ts
@@ -5,7 +5,12 @@ const config: Config = {
   rootDir: './',
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
   projects: ['<rootDir>/jest.config.ts'],
-  testEnvironment: 'jsdom'
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@shared/(.*)$': '<rootDir>/src/shared/$1',
+    '^@todolist/(.*)$': '<rootDir>/src/todolist/$1',
+    '^@data/(.*)$': '<rootDir>/src/data/$1'
+  }
 };
 
 export default config;
